2b7a0a530827408a74543afb5e0d244271a2ad34
Håkon Antonsen <hakon.antonsen@telia.no>
2017-10-01 17:01:24 +0200

    Add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) =>
+    <header className="main-header">
+        <h1 className="main-header__title">FlexTimeTracker</h1>
+    </header>;
+
+export default Header;
